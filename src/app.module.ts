99f035b2fa6f9637b22d13ecd1e25b700860904f
Luiz Henrique <luiz.souza@awscode.com.br>
2020-09-22 10:24:04 -0300

    chore: create module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
