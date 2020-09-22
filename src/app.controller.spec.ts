5f9d14119a7dd33293c264e35188351ba728ecc7
Luiz Henrique <luiz.souza@awscode.com.br>
2020-09-22 10:06:13 -0300

    chore: add eslint airbnb

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Backend Projeto Special de Pé!!! \u{1F601}"', () => {
+      expect(appController.getHello()).toBe('Backend Projeto Special de Pé!!! \u{1F601}');
     });
   });
 });
