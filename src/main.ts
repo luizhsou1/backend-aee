808795ac923c0fa77a61aeffc4ca7a4b6ce1657e
Luiz Henrique <luiz.souza@awscode.com.br>
2020-09-30 17:41:16 -0300

    feat: add setup cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { WinstonModule } from 'nest-winston';
 import { AppModule } from './app.module';
 import { winstonConfig } from './core/configs/winston.config';
+import { setupCors } from './core/cors/cors.setup';
 import { setupSwagger } from './core/swagger/swagger.setup';
 
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create(AppModule, { logger });
   app.setGlobalPrefix('api/v1');
+  setupCors(app);
   setupSwagger(app);
   await app.listen(3000);
 }
