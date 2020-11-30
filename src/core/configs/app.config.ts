b383915370eaf45f46c84fb019b2002da4d5c112
Luiz Henrique <luizhsou1@gmail.com>
2020-11-29 22:40:05 -0300

    chore: config environment of port

--- src/core/configs/app.config.ts
+++ src/core/configs/app.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   env: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.API_PORT) || 3060,
+  port: parseInt(process.env.PORT) || 3060,
 }));
